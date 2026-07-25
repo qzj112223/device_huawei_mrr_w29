8a7e9bfa99b39e1f27ebc31bafc99f5e3a95f6fa
qzj112223 <60811857+qzj112223@users.noreply.github.com>
2026-07-25 23:43:25 +0800

    1

---
+++ omni_mrr_w29.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+PRODUCT_NAME := omni_mrr_w29
+PRODUCT_DEVICE := mrr_w29
+PRODUCT_BRAND := HUAWEI
+PRODUCT_MODEL := MatePad Pro
+PRODUCT_MANUFACTURER := HUAWEI
